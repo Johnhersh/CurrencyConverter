db5c8f1356e51058e38d8dfb507687c7993158bc
John Hershberg <johnhershberg@Johns-MacBook-Pro-14814.local>
2020-07-13 13:41:57 -0700

    added comment

--- app/redux/rootReducer.ts
+++ app/redux/rootReducer.ts
@@ -1,3 +1,6 @@
+// Will probably delete this file. Ended up not needing to combine the reducers.
+// Keeping for reference in case I decide to add more reducers in the future
+
 import { combineReducers } from "redux";
 
 import currencyReducer from "./currencyReducer";
