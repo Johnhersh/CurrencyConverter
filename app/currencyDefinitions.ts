9c84cd19c7fe9496f4576b6738e56b788cc556a4
John Hershberg <johnher@gmail.com>
2020-07-24 09:34:09 -0700

    New currency icons

--- app/currencyDefinitions.ts
+++ app/currencyDefinitions.ts
@@ -39,12 +39,17 @@ export const cryptoCurrencyNames: { [name: string]: string } = {
 
 export const currencyIcons: { [name: string]: any } = {
   USD: require("../assets/CurrencyIcons/USD.png"),
+  AUD: require("../assets/CurrencyIcons/AUD.png"),
   EUR: require("../assets/CurrencyIcons/EUR.png"),
   GBP: require("../assets/CurrencyIcons/GBP.png"),
   JPY: require("../assets/CurrencyIcons/JPY.png"),
+  CHF: require("../assets/CurrencyIcons/CHF.png"),
   DKK: require("../assets/CurrencyIcons/DKK.png"),
   SEK: require("../assets/CurrencyIcons/SEK.png"),
+  ILS: require("../assets/CurrencyIcons/ILS.png"),
   CAD: require("../assets/CurrencyIcons/CAD.png"),
+  ISK: require("../assets/CurrencyIcons/ISK.png"),
+  RUB: require("../assets/CurrencyIcons/RUB.png"),
   BTC: require("../assets/CurrencyIcons/BTC.png"),
   ETH: require("../assets/CurrencyIcons/ETH.png"),
 };
