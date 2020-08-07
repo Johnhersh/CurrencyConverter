26c25eb08cc33caa8ac27bebfec462dad5ff2a1e
John Hershberg <johnher@gmail.com>
2020-08-06 22:31:40 -0700

    Added styling to tab bar

--- App.tsx
+++ App.tsx
@@ -9,13 +9,35 @@ import ManageCurrenciesView from "./app/views/ManageCurrenciesView";
 
 import store from "./app/redux/store";
 
+import { FontAwesome5 } from "@expo/vector-icons";
+
+<FontAwesome5 name="list" size={24} color="black" />;
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home">
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => {
+              let iconName = "";
+              let iconColor = "black";
+
+              if (route.name === "Home") {
+                iconName = focused ? "money-bill-wave" : "money-bill-wave-alt";
+                iconColor = focused ? "#00B4FF" : "black";
+              } else if (route.name === "Manage") {
+                iconName = "list";
+                iconColor = focused ? "#00B4FF" : "black";
+              }
+
+              return <FontAwesome5 name={iconName} size={24} color={iconColor} />;
+            },
+          })}
+        >
           <Tab.Screen name="Home" component={HomeView} options={{ title: "Currency Converter" }} />
           <Tab.Screen
             name="Manage"
